b210e365910f60bac5ac809693600e49cd76ce74
Moorteza81 <morteza_hoseyni81@yahoo.com>
2022-02-06 23:15:58 +0330

    SentReference -> Form added + UI Design

---
+++ AutomationSystem/UserPanel/frmUserSentReferenceLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataModelLayer.Models;
+using AutomationSystem.Moduls;
+
+namespace AutomationSystem.UserPanel
+{
+    public partial class frmUserSentReferenceLetters : Form
+    {
+        public frmUserSentReferenceLetters()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUserSentReferenceLetters_Load(object sender, EventArgs e)
+        {
+            this.Left = 10;
+            this.Top = 160;
+        }
+    }
+}
