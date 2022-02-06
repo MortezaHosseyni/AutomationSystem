d11a32d64e168a7f35e574c443b853eb665bd842
Moorteza81 <morteza_hoseyni81@yahoo.com>
2022-02-06 17:42:28 +0330

    RecivedReferenceLetters -> Form added + UI Design

---
+++ AutomationSystem/UserPanel/frmUserRecivedReferenceLetters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataModelLayer.Models;
+using AutomationSystem.Moduls;
+
+namespace AutomationSystem.UserPanel
+{
+    public partial class frmUserRecivedReferenceLetters : Form
+    {
+        Office_Automation_DatabaseEntities db = new Office_Automation_DatabaseEntities();
+        public frmUserRecivedReferenceLetters()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUserRecivedReferenceLetters_Load(object sender, EventArgs e)
+        {
+            this.Left = 10;
+            this.Top = 160;
+        }
+    }
+}
