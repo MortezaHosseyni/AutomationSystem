4d0b9e8e595acff2cf965e2935a85705acaec56b
Moorteza81 <morteza_hoseyni81@yahoo.com>
2022-02-09 22:25:17 +0330

    UserUpdatePassword -> Form added + UI Design

--- AutomationSystem/UserPanel/frmUserUpdatePassword.Designer.cs
+++ AutomationSystem/UserPanel/frmUserUpdatePassword.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace AutomationSystem.AdminPanel
+﻿namespace AutomationSystem.UserPanel
 {
-    partial class frmUpdatePassword
+    partial class frmUserUpdatePassword
     {
         /// <summary>
         /// Required designer variable.
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_Title = new DevComponents.DotNetBar.LabelX();
             this.pnl_Main = new DevComponents.DotNetBar.PanelEx();
             this.pnl_Buttons = new DevComponents.DotNetBar.Controls.GroupPanel();
             this.btn_Exit = new DevComponents.DotNetBar.ButtonX();
@@ -37,11 +38,26 @@
             this.txt_NewPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.lbl_ConfirmNewPassword = new DevComponents.DotNetBar.LabelX();
             this.lbl_NewPassword = new DevComponents.DotNetBar.LabelX();
-            this.lbl_Title = new DevComponents.DotNetBar.LabelX();
+            this.lbl_OldPassword = new DevComponents.DotNetBar.LabelX();
+            this.txt_OldPassword = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.pnl_Main.SuspendLayout();
             this.pnl_Buttons.SuspendLayout();
             this.pnl_ChangePassword.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            // 
+            // 
+            // 
+            this.lbl_Title.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lbl_Title.Font = new System.Drawing.Font("B Nazanin", 18.25F);
+            this.lbl_Title.Location = new System.Drawing.Point(12, 12);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(579, 39);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "...";
+            this.lbl_Title.TextAlignment = System.Drawing.StringAlignment.Center;
             // 
             // pnl_Main
             // 
@@ -62,7 +78,7 @@
             this.pnl_Main.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
             this.pnl_Main.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
             this.pnl_Main.Style.GradientAngle = 90;
-            this.pnl_Main.TabIndex = 0;
+            this.pnl_Main.TabIndex = 4;
             // 
             // pnl_Buttons
             // 
@@ -130,14 +146,15 @@
             this.btn_Save.Symbol = "";
             this.btn_Save.TabIndex = 0;
             this.btn_Save.Text = "ثبت اطلاعات";
-            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
             // 
             // pnl_ChangePassword
             // 
             this.pnl_ChangePassword.CanvasColor = System.Drawing.SystemColors.Control;
             this.pnl_ChangePassword.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
             this.pnl_ChangePassword.Controls.Add(this.txt_ConfirmNewPassword);
+            this.pnl_ChangePassword.Controls.Add(this.txt_OldPassword);
             this.pnl_ChangePassword.Controls.Add(this.txt_NewPassword);
+            this.pnl_ChangePassword.Controls.Add(this.lbl_OldPassword);
             this.pnl_ChangePassword.Controls.Add(this.lbl_ConfirmNewPassword);
             this.pnl_ChangePassword.Controls.Add(this.lbl_NewPassword);
             this.pnl_ChangePassword.DisabledBackColor = System.Drawing.Color.Empty;
@@ -181,12 +198,12 @@
             // 
             this.txt_ConfirmNewPassword.Border.Class = "TextBoxBorder";
             this.txt_ConfirmNewPassword.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.txt_ConfirmNewPassword.Location = new System.Drawing.Point(68, 81);
+            this.txt_ConfirmNewPassword.Location = new System.Drawing.Point(64, 106);
             this.txt_ConfirmNewPassword.Name = "txt_ConfirmNewPassword";
             this.txt_ConfirmNewPassword.PasswordChar = '*';
             this.txt_ConfirmNewPassword.PreventEnterBeep = true;
             this.txt_ConfirmNewPassword.Size = new System.Drawing.Size(330, 36);
-            this.txt_ConfirmNewPassword.TabIndex = 2;
+            this.txt_ConfirmNewPassword.TabIndex = 3;
             this.txt_ConfirmNewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txt_ConfirmNewPassword.WatermarkText = "تكرار گذرواژه جديد...";
             // 
@@ -197,12 +214,12 @@
             // 
             this.txt_NewPassword.Border.Class = "TextBoxBorder";
             this.txt_NewPassword.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.txt_NewPassword.Location = new System.Drawing.Point(68, 39);
+            this.txt_NewPassword.Location = new System.Drawing.Point(64, 64);
             this.txt_NewPassword.Name = "txt_NewPassword";
             this.txt_NewPassword.PasswordChar = '*';
             this.txt_NewPassword.PreventEnterBeep = true;
             this.txt_NewPassword.Size = new System.Drawing.Size(330, 36);
-            this.txt_NewPassword.TabIndex = 1;
+            this.txt_NewPassword.TabIndex = 2;
             this.txt_NewPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txt_NewPassword.WatermarkText = "گذرواژه جديد...";
             // 
@@ -213,7 +230,7 @@
             // 
             // 
             this.lbl_ConfirmNewPassword.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.lbl_ConfirmNewPassword.Location = new System.Drawing.Point(404, 84);
+            this.lbl_ConfirmNewPassword.Location = new System.Drawing.Point(400, 109);
             this.lbl_ConfirmNewPassword.Name = "lbl_ConfirmNewPassword";
             this.lbl_ConfirmNewPassword.Size = new System.Drawing.Size(118, 33);
             this.lbl_ConfirmNewPassword.TabIndex = 0;
@@ -226,27 +243,42 @@
             // 
             // 
             this.lbl_NewPassword.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.lbl_NewPassword.Location = new System.Drawing.Point(404, 42);
+            this.lbl_NewPassword.Location = new System.Drawing.Point(400, 67);
             this.lbl_NewPassword.Name = "lbl_NewPassword";
             this.lbl_NewPassword.Size = new System.Drawing.Size(85, 33);
             this.lbl_NewPassword.TabIndex = 0;
             this.lbl_NewPassword.Text = "گذرواژه جديد:";
             // 
-            // lbl_Title
+            // lbl_OldPassword
+            // 
+            this.lbl_OldPassword.AutoSize = true;
+            // 
+            // 
+            // 
+            this.lbl_OldPassword.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lbl_OldPassword.Location = new System.Drawing.Point(400, 25);
+            this.lbl_OldPassword.Name = "lbl_OldPassword";
+            this.lbl_OldPassword.Size = new System.Drawing.Size(80, 33);
+            this.lbl_OldPassword.TabIndex = 0;
+            this.lbl_OldPassword.Text = "گذرواژه قبلي:";
+            // 
+            // txt_OldPassword
             // 
             // 
             // 
             // 
-            this.lbl_Title.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.lbl_Title.Font = new System.Drawing.Font("B Nazanin", 18.25F);
-            this.lbl_Title.Location = new System.Drawing.Point(12, 12);
-            this.lbl_Title.Name = "lbl_Title";
-            this.lbl_Title.Size = new System.Drawing.Size(579, 39);
-            this.lbl_Title.TabIndex = 0;
-            this.lbl_Title.Text = "...";
-            this.lbl_Title.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.txt_OldPassword.Border.Class = "TextBoxBorder";
+            this.txt_OldPassword.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txt_OldPassword.Location = new System.Drawing.Point(64, 22);
+            this.txt_OldPassword.Name = "txt_OldPassword";
+            this.txt_OldPassword.PasswordChar = '*';
+            this.txt_OldPassword.PreventEnterBeep = true;
+            this.txt_OldPassword.Size = new System.Drawing.Size(330, 36);
+            this.txt_OldPassword.TabIndex = 1;
+            this.txt_OldPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_OldPassword.WatermarkText = "گذرواژه قبلي...";
             // 
-            // frmUpdatePassword
+            // frmUserUpdatePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -255,11 +287,11 @@
             this.Controls.Add(this.pnl_Main);
             this.Font = new System.Drawing.Font("B Nazanin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6);
-            this.Name = "frmUpdatePassword";
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmUserUpdatePassword";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Load += new System.EventHandler(this.frmUpdatePassword_Load);
+            this.Load += new System.EventHandler(this.frmUserUpdatePassword_Load);
             this.pnl_Main.ResumeLayout(false);
             this.pnl_Buttons.ResumeLayout(false);
             this.pnl_ChangePassword.ResumeLayout(false);
@@ -270,15 +302,17 @@
 
         #endregion
 
+        public DevComponents.DotNetBar.LabelX lbl_Title;
         private DevComponents.DotNetBar.PanelEx pnl_Main;
         private DevComponents.DotNetBar.Controls.GroupPanel pnl_Buttons;
-        private DevComponents.DotNetBar.Controls.GroupPanel pnl_ChangePassword;
-        private DevComponents.DotNetBar.LabelX lbl_ConfirmNewPassword;
-        private DevComponents.DotNetBar.LabelX lbl_NewPassword;
-        private DevComponents.DotNetBar.Controls.TextBoxX txt_NewPassword;
-        private DevComponents.DotNetBar.Controls.TextBoxX txt_ConfirmNewPassword;
         private DevComponents.DotNetBar.ButtonX btn_Exit;
         private DevComponents.DotNetBar.ButtonX btn_Save;
-        public DevComponents.DotNetBar.LabelX lbl_Title;
+        private DevComponents.DotNetBar.Controls.GroupPanel pnl_ChangePassword;
+        private DevComponents.DotNetBar.Controls.TextBoxX txt_ConfirmNewPassword;
+        private DevComponents.DotNetBar.Controls.TextBoxX txt_NewPassword;
+        private DevComponents.DotNetBar.LabelX lbl_ConfirmNewPassword;
+        private DevComponents.DotNetBar.LabelX lbl_NewPassword;
+        private DevComponents.DotNetBar.Controls.TextBoxX txt_OldPassword;
+        private DevComponents.DotNetBar.LabelX lbl_OldPassword;
     }
 }
---
+++ AutomationSystem/UserPanel/frmUserUpdatePassword.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataModelLayer.Models;
+using AutomationSystem.Moduls;
+
+namespace AutomationSystem.UserPanel
+{
+    public partial class frmUserUpdatePassword : Form
+    {
+        Office_Automation_DatabaseEntities db = new Office_Automation_DatabaseEntities();
+        public frmUserUpdatePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void frmUserUpdatePassword_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
